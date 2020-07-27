117645dc7515a3a2057c13810ea1fdc8edadb4c4
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2020-07-28 01:44:25 +0300

    Retries audit

---
+++ src/Swisschain.Extensions.MassTransit/Audit/MessageRetryAuditObserver.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using GreenPipes;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Swisschain.Extensions.MassTransit.Audit
+{
+    sealed class MessageRetryAuditObserver : IRetryObserver
+    {
+        private readonly ILogger<MessageRetryAuditObserver> _logger;
+
+        public MessageRetryAuditObserver(ILogger<MessageRetryAuditObserver> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task PostCreate<T>(RetryPolicyContext<T> context) where T : class, PipeContext
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task PostFault<T>(RetryContext<T> context) where T : class, PipeContext
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task PreRetry<T>(RetryContext<T> context) where T : class, PipeContext
+        {
+            var c = (ConsumeContext)context.Context;
+
+            _logger.LogInformation("Message is being retried {@context}", new
+            {
+                MessageId = c.MessageId,
+                RetryAttempt = context.RetryAttempt
+            });
+
+            return Task.CompletedTask;
+        }
+
+        public Task RetryFault<T>(RetryContext<T> context) where T : class, PipeContext
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task RetryComplete<T>(RetryContext<T> context) where T : class, PipeContext
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Swisschain.Extensions.MassTransit/RetryConfiguratorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GreenPipes.Configurators;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swisschain.Extensions.MassTransit.Audit;
+
+namespace Swisschain.Extensions.MassTransit
+{
+    public static class RetryConfiguratorExtensions
+    {
+        public static IRetryConfigurator AddRetriesAudit(this IRetryConfigurator configurator, IServiceProvider provider)
+        {
+            configurator.ConnectRetryObserver(
+                new MessageRetryAuditObserver(
+                    provider.GetRequiredService<ILogger<MessageRetryAuditObserver>>()));
+
+            return configurator;
+        }
+    }
+}
