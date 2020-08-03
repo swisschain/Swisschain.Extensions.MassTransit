c661f732bc6182ac1d0da788f45167cb3ef0a835
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2020-08-03 15:48:31 +0300

    Added default receiving endpoint retries configuration observer

---
+++ src/Swisschain.Extensions.MassTransit/EndpointConfigurationObserverConnectorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit.EndpointConfigurators;
+using Swisschain.Extensions.MassTransit.Retries;
+
+namespace Swisschain.Extensions.MassTransit
+{
+    public static class EndpointConfigurationObserverConnectorExtensions
+    {
+        public static IEndpointConfigurationObserverConnector UseDefaultRetries(this IEndpointConfigurationObserverConnector connector)
+        {
+            connector.ConnectEndpointConfigurationObserver(new DefaultReceiveEndpointRetriesConfigurationObserver());
+
+            return connector;
+        }
+    }
+}
---
+++ src/Swisschain.Extensions.MassTransit/Retries/DefaultReceiveEndpointRetriesConfigurationObserver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using GreenPipes;
+using MassTransit;
+using MassTransit.EndpointConfigurators;
+
+namespace Swisschain.Extensions.MassTransit.Retries
+{
+    internal class DefaultReceiveEndpointRetriesConfigurationObserver : IEndpointConfigurationObserver
+    {
+        public void EndpointConfigured<T>(T configurator) where T : IReceiveEndpointConfigurator
+        {
+            configurator.UseScheduledRedelivery(r =>
+                r.Intervals(
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(30),
+                    TimeSpan.FromSeconds(60),
+                    TimeSpan.FromMinutes(10),
+                    TimeSpan.FromMinutes(30)));
+
+            configurator.UseMessageRetry(y =>
+            {
+                y.Intervals(
+                    TimeSpan.FromMilliseconds(0),
+                    TimeSpan.FromMilliseconds(100),
+                    TimeSpan.FromMilliseconds(200),
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(3));
+            });
+        }
+    }
+}
