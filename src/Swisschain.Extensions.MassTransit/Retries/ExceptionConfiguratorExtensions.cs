510ff599a57b0876c06bb27453841611efd29a9b
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2020-11-23 12:28:37 +0300

    Improved retries configurability

---
+++ src/Swisschain.Extensions.MassTransit/Retries/ExceptionConfiguratorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GreenPipes.Configurators;
+
+namespace Swisschain.Extensions.MassTransit.Retries
+{
+    public static class ExceptionConfiguratorExtensions
+    {
+        public static IExceptionConfigurator UseDefaultExceptionFilters(this IExceptionConfigurator configurator)
+        {
+            configurator.Ignore<ArgumentException>();
+            configurator.Ignore<NullReferenceException>();
+            configurator.Ignore<InvalidCastException>();
+            configurator.Ignore<IndexOutOfRangeException>();
+            configurator.Ignore<NotSupportedException>();
+            configurator.Ignore<NotImplementedException>();
+
+            return configurator;
+        }
+    }
+}
